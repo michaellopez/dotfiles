ac78b7a1fd75b701ae720b791117cf6cf5f055e9
Michael Zetterberg fd. Lopez <michael@weahead.se>
2016-11-22 15:28:51 +0100

    Fix nanorc build.

--- nano/build.sh
+++ nano/build.sh
@@ -6,4 +6,4 @@ TOKEN="~/.nano"
 REPLACE="${DOTFILES_DIR}/nano/nanorc"
 
 cat ${DOTFILES_DIR}/nano/nanorc/nanorc | sed -e s@${TOKEN}@${REPLACE}@g > ${DOTFILES_DIR}/nano/.nanorc
-cat ${DOTFILES_DIR}/nano/settings >> ./.nanorc
+cat ${DOTFILES_DIR}/nano/settings >> ${DOTFILES_DIR}/nano/.nanorc
