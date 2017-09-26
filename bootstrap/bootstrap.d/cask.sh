2275eac7c7bdf3d9be9e324c13cb1d71ee499b9a
Michael Zetterberg fd. Lopez <michael@weahead.se>
2017-09-26 23:42:05 +0200

    Added bootstrap file for casks.

---
+++ bootstrap/bootstrap.d/cask.sh
@@ -0,0 +1,24 @@
+brew cask install \
+	alfred \
+	appcleaner \
+	choosy \
+	diffmerge \
+	fluid \
+	google-chrome \
+	google-drive \
+	grandperspective \
+	iterm2 \
+	keybase \
+	lastpass \
+	little-snitch \
+	phpstorm \
+	sizeup \
+	slack \
+	sonos \
+	the-unarchiver \
+	toggldesktop \
+	yujitach-menumeters
+
+open '/usr/local/Caskroom/lastpass/latest/LastPass Installer.app'
+open '/usr/local/Caskroom/little-snitch/3.7.3/Little Snitch Installer.app'
+
